005d5b0db0c5386ca2d4f4e433964c3b9eccad7c
Rohit <rkkanojia7@gmail,com>
2018-05-27 23:18:43 +0530

    Initial commit

---
+++ app/src/main/java/in/eyehunt/constraintlayout/LoginActivity.kt
@@ -0,0 +1,15 @@
+package `in`.eyehunt.constraintlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * A login screen that offers login via email/password.
+ */
+class LoginActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
